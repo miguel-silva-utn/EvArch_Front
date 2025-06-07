e0a86d0ba84c8eb44845cf6754652eef1d0f49a6
Miguel Silva C. <miguel.silva@inspt.utn.edu.ar>
2025-06-07 14:19:45 -0300

    Evaluador de Archivos C y Pseint - Frontend - v1.0

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://localhost:3434/api',
+    baseURL: 'https://e0f7-2800-810-52a-dc0-544b-ec9-1b2c-d8c4.ngrok-free.app/api',
+    //baseURL: 'http://localhost:3434/api',
 });
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0', // acepta conexiones externas
+    port: 5173,
+    strictPort: true,
+    allowedHosts: 'all' // permite cualquier dominio, incluido Ngrok
+  }
 })
