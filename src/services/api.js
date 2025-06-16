dc468c32390ea01927bf55d27d9d7ad34ee50abc
Miguel Silva C. <miguel.silva@inspt.utn.edu.ar>
2025-06-16 16:11:23 -0300

    Evaluador de Archivos C y Pseint - Frontend - v1.0

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'https://e0f7-2800-810-52a-dc0-544b-ec9-1b2c-d8c4.ngrok-free.app/api',
+    baseURL: 'https://2e09-2800-810-52a-dc0-a082-65ec-cb17-9450.ngrok-free.app/api',
     //baseURL: 'http://localhost:3434/api',
 });
